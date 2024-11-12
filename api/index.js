2f60ea8a49b4c0868776bcedb7a14383538ec4ba
RenanlMelo <renanlaramelo@gmail.com>
2024-11-11 23:34:14 -0300

    Atualizando para vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
@@ -11,17 +12,48 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
+app.use("/images", express.static(path.join(__dirname, "../images")));
+
 app.get("/api/image-metadata", (req, res) => {
   const imageMetadata = [
-    { name: "Costume", preco: "1540", type: "image/jpg", url: "/images/1.jpg" },
-    { name: "Terno", preco: "2800", type: "image/jpg", url: "/images/2.jpg" },
-    { name: "Sapatenis", preco: "290", type: "image/jpg", url: "/images/3.jpg" },
-    { name: "Camisas", preco: "250", type: "image/jpg", url: "/images/4.jpg" },
-    { name: "Camisetas gola careca de malha", preco: "60", type: "image/jpg", url: "/images/5.jpg" }
+    {
+      name: "Costume",
+      preco: "1540",
+      type: "image/jpg",
+      url: "/images/1.jpg",
+      filter: "costume",
+    },
+    {
+      name: "Terno",
+      preco: "2800",
+      type: "image/jpg",
+      url: "/images/2.jpg",
+      filter: "terno",
+    },
+    {
+      name: "Sapatenis",
+      preco: "290",
+      type: "image/jpg",
+      url: "/images/3.jpg",
+      filter: "sapatenis",
+    },
+    {
+      name: "Camisas",
+      preco: "250",
+      type: "image/jpg",
+      url: "/images/4.jpg",
+      filter: "camisa",
+    },
+    {
+      name: "Camisetas gola careca de malha",
+      preco: "60",
+      type: "image/jpg",
+      url: "/images/5.jpg",
+      filter: "camiseta",
+    },
   ];
   res.json(imageMetadata);
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
